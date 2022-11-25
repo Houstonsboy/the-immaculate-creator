22a210a6ce21e60f141ab7c7b5dd3dfb6660a2a0
Houstonsboy <86203270+Houstonsboy@users.noreply.github.com>
2022-11-25 00:52:32 -0800

    Add files via upload

---
+++ src/views/SpotifyCuratedView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="body">
+   <h1>SpotifyCurated</h1>
+   <p>welcome ladies and gentlemen<br> this is your captain speaking<br> prepare for the voyage</p>
+  </div>   
+
+
+</template>
+
+<style >
+body{
+    background-image: url("../assets/Travis Scott on Amazon Music.jpg");
+    position:relative;
+    background-repeat: no-repeat;
+    background-size:cover;
+    
+}
+h1{
+    text-align:center;
+    color:green;
+}
+</style>
+
+
+<script>
+  export default{
+    name: "SpotifyCurated"
+    
+  };
+</script>
---
+++ src/views/TodoListView.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="TodoListView">
+<h1>TO DO LIST</h1>
+<p>welcome to your my do listAPP</p>
+
+<label name="date">date
+<input type="date" required name="date">
+</label>
+
+<label name="location">location
+<input  required name="location">
+</label>
+
+<label name="task">task
+<input  required name="task">
+</label>
+
+<label name="people">people
+<input  required name="people">
+</label>
+<label name="submit">submit
+<input type="submit" required name="submit">
+</label>
+</div>
+</template>
+
+<style scoped>
+h1{
+    text-align:center;
+}
+</style>
+
+<script>
+  
+  export default{
+    name: "TodoListView",
+    
+  };
+</script>
