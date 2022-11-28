fb5bcbe23b547d2b7075e1f1fdd6d4b47ae6a42c
Houstonsboy <86203270+Houstonsboy@users.noreply.github.com>
2022-11-28 07:22:45 -0800

    Add files via upload
    this version contains the playlist and two asap rocky psychedelic songs from spotify

--- src/views/SpotifyCuratedView.vue
+++ src/views/SpotifyCuratedView.vue
@@ -2,18 +2,36 @@
   <div class="body">
    <h1>SpotifyCurated</h1>
    <p>welcome ladies and gentlemen<br> this is your captain speaking<br> prepare for the voyage</p>
+
+   <div id="tyler the creator">
+   <h2>Psychedelic experience</h2>
+   <p>your personalised introduction to a different dimension</p>
+   <ul>
+   <li><iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/6J9FgTr3z44Bw6ABeVL415?utm_source=generator" width="50%" height="150" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></li>
+   <li><iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/2WxzLbJfPA2vJGxQiKd7Jq?utm_source=generator" width="50%" height="150" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></li>
+   <p>and your own curated playlist</p>
+
+   <li><iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37B7dCdKyeofAqZjrO60Yi?utm_source=generator" width="70%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></li>
+   </ul>
+   
+   </div>
   </div>   
 
 
 </template>
 
-<style >
+<style scoped>
 body{
-    background-image: url("../assets/Travis Scott on Amazon Music.jpg");
+ 
     position:relative;
     background-repeat: no-repeat;
     background-size:cover;
     
+}
+ul{
+    list-style:none;
+    display:block;
+    margin:10px;
 }
 h1{
     text-align:center;
@@ -27,4 +45,5 @@ h1{
     name: "SpotifyCurated"
     
   };
+    
 </script>
